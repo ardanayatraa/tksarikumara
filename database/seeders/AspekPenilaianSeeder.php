93275ec781a41b430b9df4908a5d0bfcce342751
Ardana Yatra <imadeardanayatra@gmail.com>
2025-05-27 18:02:11 +0800

    update v1

--- database/seeders/AspekPenilaianSeeder.php
+++ database/seeders/AspekPenilaianSeeder.php
@@ -3,18 +3,12 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use App\Models\AspekPenilaian;
 
 class AspekPenilaianSeeder extends Seeder
 {
     public function run()
     {
-        // nonaktifkan cek foreign key, truncate, lalu aktifkan kembali
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        AspekPenilaian::truncate();
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
-
         $ageRanges = [
             ['min' => 2, 'max' => 3],
             ['min' => 3, 'max' => 4],
@@ -22,91 +16,181 @@ class AspekPenilaianSeeder extends Seeder
             ['min' => 5, 'max' => 6],
         ];
 
-        $aspects = [
-            [
-                'kode'     => 'I',
-                'nama'     => 'Nilai Agama dan Moral',
-                'kategori' => 'Spiritual',
-                'children' => [],
-            ],
-            [
-                'kode'     => 'II',
-                'nama'     => 'Fisik - Motorik',
-                'kategori' => 'Motorik',
-                'children' => [
-                    ['kode' => 'II.A', 'nama' => 'Motorik Kasar', 'kategori' => 'Motorik'],
-                    ['kode' => 'II.B', 'nama' => 'Motorik Halus', 'kategori' => 'Motorik'],
-                    ['kode' => 'II.C', 'nama' => 'Kesehatan dan Perilaku Keselamatan', 'kategori' => 'Kesehatan'],
-                ],
-            ],
-            [
-                'kode'     => 'III',
-                'nama'     => 'Kognitif',
-                'kategori' => 'Kognitif',
-                'children' => [
-                    ['kode' => 'III.A', 'nama' => 'Belajar dan Memecahkan Masalah', 'kategori' => 'Kognitif'],
-                    ['kode' => 'III.B', 'nama' => 'Berpikir Logis', 'kategori' => 'Kognitif'],
-                    ['kode' => 'III.C', 'nama' => 'Berpikir Simbolik', 'kategori' => 'Kognitif'],
-                ],
-            ],
-            [
-                'kode'     => 'IV',
-                'nama'     => 'Bahasa',
-                'kategori' => 'Bahasa',
-                'children' => [
-                    ['kode' => 'IV.A', 'nama' => 'Memahami Bahasa', 'kategori' => 'Bahasa'],
-                    ['kode' => 'IV.B', 'nama' => 'Mengungkapkan Bahasa', 'kategori' => 'Bahasa'],
-                ],
-            ],
-            [
-                'kode'     => 'V',
-                'nama'     => 'Sosial - Emosional',
-                'kategori' => 'Sosial',
-                'children' => [
-                    ['kode' => 'V.A', 'nama' => 'Kesadaran Diri', 'kategori' => 'Sosial'],
-                    ['kode' => 'V.B', 'nama' => 'Tanggung Jawab Diri dan Orang Lain', 'kategori' => 'Sosial'],
-                    ['kode' => 'V.C', 'nama' => 'Perilaku Prososial', 'kategori' => 'Sosial'],
-                ],
-            ],
-            [
-                'kode'     => 'VI',
-                'nama'     => 'Seni',
-                'kategori' => 'Seni',
-                'children' => [
-                    ['kode' => 'VI.A', 'nama' => 'Mampu membedakan bunyi dan suara', 'kategori' => 'Seni'],
-                    ['kode' => 'VI.B', 'nama' => 'Ingin mengikuti kegiatan musik, orang dan hewan', 'kategori' => 'Seni'],
-                    ['kode' => 'VI.C', 'nama' => 'Ingin mengikuti kegiatan seni', 'kategori' => 'Seni'],
-                ],
-            ],
-        ];
-
         foreach ($ageRanges as $range) {
             $min = $range['min'];
             $max = $range['max'];
 
-            foreach ($aspects as $asp) {
-                // buat parent
-                $parent = AspekPenilaian::create([
-                    'kode_aspek' => $asp['kode'],
-                    'nama_aspek' => $asp['nama'],
-                    'kategori'   => $asp['kategori'],
-                    'parent_id'  => null,
-                    'min_umur'   => $min,
-                    'max_umur'   => $max,
-                ]);
+            // I. Nilai Agama dan Moral
+            AspekPenilaian::create([
+                'kode_aspek' => 'I',
+                'nama_aspek' => 'Nilai Agama dan Moral',
+                'kategori'   => 'Spiritual',
+                'parent_id'  => null,
+                'min_umur'   => $min,
+                'max_umur'   => $max,
+            ]);
 
-                // buat tiap sub-aspek
-                foreach ($asp['children'] as $child) {
-                    AspekPenilaian::create([
-                        'kode_aspek' => $child['kode'],
-                        'nama_aspek' => $child['nama'],
-                        'kategori'   => $child['kategori'],
-                        'parent_id'  => $parent->id_aspek,
-                        'min_umur'   => $min,
-                        'max_umur'   => $max,
-                    ]);
-                }
-            }
+            // II. Fisik – Motorik
+            $fisik = AspekPenilaian::create([
+                'kode_aspek' => 'II',
+                'nama_aspek' => 'Fisik - Motorik',
+                'kategori'   => 'Motorik',
+                'parent_id'  => null,
+                'min_umur'   => $min,
+                'max_umur'   => $max,
+            ]);
+            AspekPenilaian::create([
+                'kode_aspek' => 'II.A',
+                'nama_aspek' => 'Motorik Kasar',
+                'kategori'   => 'Motorik',
+                'parent_id'  => $fisik->id_aspek,
+                'min_umur'   => $min,
+                'max_umur'   => $max,
+            ]);
+            AspekPenilaian::create([
+                'kode_aspek' => 'II.B',
+                'nama_aspek' => 'Motorik Halus',
+                'kategori'   => 'Motorik',
+                'parent_id'  => $fisik->id_aspek,
+                'min_umur'   => $min,
+                'max_umur'   => $max,
+            ]);
+            AspekPenilaian::create([
+                'kode_aspek' => 'II.C',
+                'nama_aspek' => 'Kesehatan dan Perilaku Keselamatan',
+                'kategori'   => 'Kesehatan',
+                'parent_id'  => $fisik->id_aspek,
+                'min_umur'   => $min,
+                'max_umur'   => $max,
+            ]);
+
+            // III. Kognitif
+            $kognitif = AspekPenilaian::create([
+                'kode_aspek' => 'III',
+                'nama_aspek' => 'Kognitif',
+                'kategori'   => 'Kognitif',
+                'parent_id'  => null,
+                'min_umur'   => $min,
+                'max_umur'   => $max,
+            ]);
+            AspekPenilaian::create([
+                'kode_aspek' => 'III.A',
+                'nama_aspek' => 'Belajar dan Memecahkan Masalah',
+                'kategori'   => 'Kognitif',
+                'parent_id'  => $kognitif->id_aspek,
+                'min_umur'   => $min,
+                'max_umur'   => $max,
+            ]);
+            AspekPenilaian::create([
+                'kode_aspek' => 'III.B',
+                'nama_aspek' => 'Berpikir Logis',
+                'kategori'   => 'Kognitif',
+                'parent_id'  => $kognitif->id_aspek,
+                'min_umur'   => $min,
+                'max_umur'   => $max,
+            ]);
+            AspekPenilaian::create([
+                'kode_aspek' => 'III.C',
+                'nama_aspek' => 'Berpikir Simbolik',
+                'kategori'   => 'Kognitif',
+                'parent_id'  => $kognitif->id_aspek,
+                'min_umur'   => $min,
+                'max_umur'   => $max,
+            ]);
+
+            // IV. Bahasa
+            $bahasa = AspekPenilaian::create([
+                'kode_aspek' => 'IV',
+                'nama_aspek' => 'Bahasa',
+                'kategori'   => 'Bahasa',
+                'parent_id'  => null,
+                'min_umur'   => $min,
+                'max_umur'   => $max,
+            ]);
+            AspekPenilaian::create([
+                'kode_aspek' => 'IV.A',
+                'nama_aspek' => 'Memahami Bahasa',
+                'kategori'   => 'Bahasa',
+                'parent_id'  => $bahasa->id_aspek,
+                'min_umur'   => $min,
+                'max_umur'   => $max,
+            ]);
+            AspekPenilaian::create([
+                'kode_aspek' => 'IV.B',
+                'nama_aspek' => 'Mengungkapkan Bahasa',
+                'kategori'   => 'Bahasa',
+                'parent_id'  => $bahasa->id_aspek,
+                'min_umur'   => $min,
+                'max_umur'   => $max,
+            ]);
+
+            // V. Sosial - Emosional
+            $sosial = AspekPenilaian::create([
+                'kode_aspek' => 'V',
+                'nama_aspek' => 'Sosial - Emosional',
+                'kategori'   => 'Sosial',
+                'parent_id'  => null,
+                'min_umur'   => $min,
+                'max_umur'   => $max,
+            ]);
+            AspekPenilaian::create([
+                'kode_aspek' => 'V.A',
+                'nama_aspek' => 'Kesadaran Diri',
+                'kategori'   => 'Sosial',
+                'parent_id'  => $sosial->id_aspek,
+                'min_umur'   => $min,
+                'max_umur'   => $max,
+            ]);
+            AspekPenilaian::create([
+                'kode_aspek' => 'V.B',
+                'nama_aspek' => 'Tanggung Jawab Diri dan Orang Lain',
+                'kategori'   => 'Sosial',
+                'parent_id'  => $sosial->id_aspek,
+                'min_umur'   => $min,
+                'max_umur'   => $max,
+            ]);
+            AspekPenilaian::create([
+                'kode_aspek' => 'V.C',
+                'nama_aspek' => 'Perilaku Prososial',
+                'kategori'   => 'Sosial',
+                'parent_id'  => $sosial->id_aspek,
+                'min_umur'   => $min,
+                'max_umur'   => $max,
+            ]);
+
+            // VI. Seni
+            $seni = AspekPenilaian::create([
+                'kode_aspek' => 'VI',
+                'nama_aspek' => 'Seni',
+                'kategori'   => 'Seni',
+                'parent_id'  => null,
+                'min_umur'   => $min,
+                'max_umur'   => $max,
+            ]);
+            AspekPenilaian::create([
+                'kode_aspek' => 'VI.A',
+                'nama_aspek' => 'Mampu membedakan bunyi dan suara',
+                'kategori'   => 'Seni',
+                'parent_id'  => $seni->id_aspek,
+                'min_umur'   => $min,
+                'max_umur'   => $max,
+            ]);
+            AspekPenilaian::create([
+                'kode_aspek' => 'VI.B',
+                'nama_aspek' => 'Ingin mengikuti kegiatan musik, orang dan hewan',
+                'kategori'   => 'Seni',
+                'parent_id'  => $seni->id_aspek,
+                'min_umur'   => $min,
+                'max_umur'   => $max,
+            ]);
+            AspekPenilaian::create([
+                'kode_aspek' => 'VI.C',
+                'nama_aspek' => 'Ingin mengikuti kegiatan seni',
+                'kategori'   => 'Seni',
+                'parent_id'  => $seni->id_aspek,
+                'min_umur'   => $min,
+                'max_umur'   => $max,
+            ]);
         }
     }
 }
