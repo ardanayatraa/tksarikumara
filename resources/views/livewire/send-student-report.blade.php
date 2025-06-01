92bdf0c7c2ee13083a37137d198f117c94da967d
Ardana Yatra <imadeardanayatra@gmail.com>
2025-06-02 02:26:45 +0800

    fix all

--- resources/views/livewire/send-student-report.blade.php
+++ resources/views/livewire/send-student-report.blade.php
@@ -58,7 +58,7 @@
         </x-slot>
 
         <x-slot name="footer">
-            @if (!Route::currentRouteName() == 'dashboard.siswa')
+            @if (Route::currentRouteName() !== 'dashboard.siswa')
                 <x-button wire:click="sendEmail">Kirim Email</x-button>
             @endif
 
